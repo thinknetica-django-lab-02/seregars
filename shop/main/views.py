200f520e34597207ace9b9fecfc6ff7d6ba61f1c
seregars <arss_@mail.ru>
2021-03-28 18:46:22 +0300

    Added <div> and user_name to template index.html

--- shop/main/views.py
+++ shop/main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    turn_on_block = True
+    user = request.user
+    return render(request, 'main/index.html', {'turn_on': turn_on_block, 'user': user})
+
